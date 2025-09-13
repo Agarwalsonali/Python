d583c487bc004485b2c4223b1351efaa72cd85aa
Sonali Agarwal <agarwalsonali922@gmail.com>
2025-09-13 14:40:33 +0530

    Initial commit

---
+++ mega_project1- Jarvis/musicLibrary.py
@@ -0,0 +1,10 @@
+music = {
+    "pal": "https://www.youtube.com/watch?v=AbkEmIgJMcU&list=RDAbkEmIgJMcU&start_radio=1&pp=ygUPdGFsd2luZGVyIHNvbmdzoAcB",
+    "wishes": "https://www.youtube.com/watch?v=Gz38Yj09k3A&list=RDGz38Yj09k3A&start_radio=1&pp=ygUPdGFsd2luZGVyIHNvbmdzoAcB",
+    "belong": "https://www.youtube.com/watch?v=VuNIsY6JdUw&list=RDVuNIsY6JdUw&start_radio=1&pp=ygUSdGF5bG9yIHN3aWZ0IHNvbmdzoAcB",
+    "love": "https://www.youtube.com/watch?v=8xg3vE8Ie_E&list=RD8xg3vE8Ie_E&start_radio=1&pp=ygUSdGF5bG9yIHN3aWZ0IHNvbmdzoAcB",
+    "bulleya": "https://www.youtube.com/watch?v=a9Hxkc9YxGE&list=RDa9Hxkc9YxGE&start_radio=1&pp=ygUGYnVsZXlhoAcB",
+    "enchanted": "https://www.youtube.com/watch?v=uyupd2PXbSQ&list=RDuyupd2PXbSQ&start_radio=1&pp=ygUWZW5jaGFudGVkIHRheWxvciBzd2lmdKAHAQ%3D%3D"
+}
+
+newsapi = "50fe5aaa29ea401d9f30aa8481ed6060"
